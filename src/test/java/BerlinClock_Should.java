a2c7ce29e87cd43cbf4cc176c66d5304f61b904d
Eelco <jong.de.eelco@gmail.com>
2017-04-17 10:54:18 +0200

    Created test case for berlin clock class

---
+++ src/test/java/BerlinClock_Should.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.Test;
+
+/**
+ * Created by jongd on 17-4-2017.
+ */
+public class BerlinClock_Should {
+
+    @Test
+    public void createBerlinClockClass() throws Exception {
+        BerlinClock berlinClock = new BerlinClock();
+    }
+}
